c5e71600b885c772938f5f11f3606cf18821d40d
Asier Salicio <salicioasier99@gmail.com>
2023-01-12 21:01:33 +0100

    add navigation

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -11,9 +11,7 @@ import { MessageService } from '../message.service';
 export class HeroesComponent {
 
   heroes: Hero[] = [];
-  selectedHero?: Hero;
-
-  constructor(private heroService: HeroService, private messageService: MessageService) {
+  constructor(private heroService: HeroService) {
 
   }
 
@@ -24,10 +22,5 @@ export class HeroesComponent {
 
   getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add('Heroes component: Selected hero: ' + hero.id);
   }
 }
